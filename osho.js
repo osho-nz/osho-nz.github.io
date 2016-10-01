c62762884527038a432d8b6e2647e32d64bc4db9
Gary Buyn <simplegsb@gmail.com>
2016-10-01 13:20:18 +1300

    Redesigned using bootstrap!

---
+++ osho.js
@@ -0,0 +1,68 @@
+$(document).ready(function()
+{
+	initPetals();
+	initLibrary();
+});
+
+function initPetals()
+{
+	$('#osho-facebook-button').mouseover(function()
+	{
+		showPetal('facebook');
+	});
+	$('#osho-facebook-button').mouseout(function()
+	{
+		hidePetal('facebook');
+	});
+
+	$('#osho-info-button').mouseover(function()
+	{
+		showPetal('info');
+	});
+	$('#osho-info-button').mouseout(function()
+	{
+		hidePetal('info');
+	});
+
+	$('#osho-meditation-button').mouseover(function()
+	{
+		showPetal('meditation');
+	});
+	$('#osho-meditation-button').mouseout(function()
+	{
+		hidePetal('meditation');
+	});
+
+	$('#osho-library-button').mouseover(function()
+	{
+		showPetal('library');
+	});
+	$('#osho-library-button').mouseout(function()
+	{
+		hidePetal('library');
+	});
+}
+
+function initLibrary()
+{
+	$('#osho-library-tabs a').click(function(event)
+	{
+		event.preventDefault()
+		$(this).tab('show');
+	});
+}
+
+function showPetal(petal)
+{
+	$('#osho-lotus-petal-' + petal).stop().fadeIn({ 'duration': 200, 'queue': false });
+}
+
+function hidePetal(petal)
+{
+	$('#osho-lotus-petal-' + petal).stop().fadeOut({ 'duration': 200, 'queue': false });
+}
+
+function goTo(section)
+{
+	$('html, body').animate({scrollTop: $('#osho-' + section).offset().top}, 'slow');
+}
