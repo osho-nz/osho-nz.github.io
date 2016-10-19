8453f6486f4f9182d6fe96e6041dfff4836c2a7d
Prem Gyan <simplegsb@gmail.com>
2016-10-20 07:50:57 +1300

    Use google calendar to provide meditation information!

--- osho.js
+++ osho.js
@@ -4,6 +4,101 @@ $(document).ready(function()
 	initLibrary();
 });
 
+function initGoogleClient()
+{
+	gapi.auth.authorize(
+	{
+		client_id: '754846761819-b1vpa018ddhcl73g6a5a90kpj53kemss.apps.googleusercontent.com',
+		immediate: true,
+		scope: 'https://www.googleapis.com/auth/calendar.readonly'
+	})
+	.then(function()
+	{
+		gapi.client.load('calendar', 'v3')
+		.then(function()
+		{
+			gapi.client.calendar.events.list(
+			{
+				'calendarId': 'info@osho.nz',
+				'maxResults': 4,
+				'timeMin': new Date().toISOString()
+			})
+			.then(function(response)
+			{
+				console.log(response);
+				response.result.items.forEach(function(event)
+				{
+					var summary = event.summary;
+					if (summary.toLowerCase().includes('chakra breath'))
+					{
+						summary = '<a href="http://www.osho.com/meditate/active-meditations/chakra-breathing-meditation" target="_blank">' + summary + '</a>';
+					}
+					else if (summary.toLowerCase().includes('chakra sound'))
+					{
+						summary = '<a href="http://www.osho.com/meditate/active-meditations/chakra-sounds-meditation" target="_blank">' + summary + '</a>';
+					}
+					else if (summary.toLowerCase().includes('devavani'))
+					{
+						summary = '<a href="http://www.osho.com/meditate/active-meditations/devavani-meditation" target="_blank">' + summary + '</a>';
+					}
+					else if (summary.toLowerCase().includes('evening meeting'))
+					{
+						summary = '<a href="http://www.osho.com/meditate/active-meditations/evening-meeting" target="_blank">' + summary + '</a>';
+					}
+					else if (summary.toLowerCase().includes('kundalini'))
+					{
+						summary = '<a href="http://www.osho.com/meditate/active-meditations/kundalini-meditation" target="_blank">' + summary + '</a>';
+					}
+					else if (summary.toLowerCase().includes('nadabrahma'))
+					{
+						summary = '<a href="http://www.osho.com/meditate/active-meditations/nadabrahma-meditation" target="_blank">' + summary + '</a>';
+					}
+					else if (summary.toLowerCase().includes('nataraj'))
+					{
+						summary = '<a href="http://www.osho.com/meditate/active-meditations/nataraj-meditation" target="_blank">' + summary + '</a>';
+					}
+					else if (summary.toLowerCase().includes('no dimension'))
+					{
+						summary = '<a href="http://www.osho.com/meditate/active-meditations/no-dimensions-meditation" target="_blank">' + summary + '</a>';
+					}
+
+					var location = event.location.substring(0, event.location.indexOf(','));
+					if (location === 'The Yoga Ground')
+					{
+						location = '<a href="http://yogaground.co.nz/" target="_blank">' + location + '</a>';
+					}
+
+					var eventHtml =
+						'<div class="panel osho-panel">' +
+							'<div class="panel-body">' +
+								'<h4>' + formatDate(new Date(event.start.dateTime)) + '</h4>' +
+								'<h3>' + summary + '</h3>';
+
+					if (event.description)
+					{
+						eventHtml +=
+								'<p>' + event.description + '</p>';
+					}
+
+					eventHtml +=
+								'<strong>Cost</strong> $5<br />' +
+								'<strong>When?</strong> ' + formatTime(new Date(event.start.dateTime)) + ' - ' + formatTime(new Date(event.end.dateTime)) + '<br>' +
+								'<strong>Where?</strong> ' + location + ' (<a href="https://www.google.com/maps?q=' + encodeURIComponent(event.location) + '" target="_blank">map</a>)' +
+							'</div>' +
+						'</div>'
+
+					$('#osho-meditation-events').append(eventHtml);
+				});
+			}, handleError);
+		}, handleError);
+	}, handleError);
+}
+
+function handleError(err)
+{
+	console.error(err);
+}
+
 function initPetals()
 {
 	$('#osho-facebook-button').mouseover(function()
@@ -66,4 +161,73 @@ function goTo(section)
 {
 	$('html, body').animate({scrollTop: $('#osho-' + section).offset().top}, 'slow');
 	hidePetal(section);
+}
+
+function formatDate(date)
+{
+	var monthNames =
+	[
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	];
+
+	var month = monthNames[date.getMonth()];
+
+	var day = date.getDate();
+	if (day === 1 || day === 21 || day === 31)
+	{
+		day += 'st';
+	}
+	else if (day === 2 || day === 22)
+	{
+		day += 'nd';
+	}
+	else if (day === 3 || day === 23)
+	{
+		day += 'rd';
+	}
+	else
+	{
+		day += 'th';
+	}
+
+	return month + ' ' + day;
+}
+
+function formatTime(date)
+{
+	var hour = date.getHours() % 12;
+	if (hour === 0)
+	{
+		hour = 12;
+	}
+
+	var minute = date.getMinutes();
+
+	var time = hour;
+	if (minute !== 0)
+	{
+		time += ':' + minute;
+	}
+
+	if (date.getHours() >= 12)
+	{
+		time += 'pm';
+	}
+	else
+	{
+		time += 'am';
+	}
+
+	return time;
 }
